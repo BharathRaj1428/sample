941ff0ccd92db9b4b8f5b19eed1a38c6e05a0e7e
BK1428bk <weacebharath07.gmail.com>
2024-09-22 22:15:53 -0700

    code added

---
+++ src/org/sample/sampleA.java
@@ -0,0 +1,17 @@
+package org.sample;
+
+public class sampleA {
+	private void Add() {
+		// TODO Auto-generated method stub
+
+	}
+	private void Sub() {
+		// TODO Auto-generated method stub
+
+	}
+	private void mul() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
